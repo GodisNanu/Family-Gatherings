3bdf2785874b34778be51786b14a35dcb265d775
Godis Nanu <godisnanu@gmail.com>
2024-11-16 20:22:12 -0500

    starting recipeData in constants.js

---
+++ constants.js
@@ -0,0 +1,62 @@
+export const recipeData = [
+  {
+    _id: 1,
+    name: "",
+    link: "",
+    recipe: "",
+  },
+  {
+    _id: 2,
+    name: "",
+    link: "",
+    recipe: "",
+  },
+  {
+    _id: 3,
+    name: "",
+    link: "",
+    recipe: "",
+  },
+  {
+    _id: 4,
+    name: "",
+    link: "",
+    recipe: "",
+  },
+  {
+    _id: 5,
+    name: "",
+    link: "",
+    recipe: "",
+  },
+  {
+    _id: 6,
+    name: "",
+    link: "",
+    recipe: "",
+  },
+  {
+    _id: 7,
+    name: "",
+    link: "",
+    recipe: "",
+  },
+  {
+    _id: 8,
+    name: "",
+    link: "",
+    recipe: "",
+  },
+  {
+    _id: 9,
+    name: "",
+    link: "",
+    recipe: "",
+  },
+  {
+    _id: 10,
+    name: "",
+    link: "",
+    recipe: "",
+  },
+];
