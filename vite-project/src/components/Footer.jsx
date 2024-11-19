caa40921dbe7710b320e60273de62fd7ddd2f309
Godis Nanu <godisnanu@gmail.com>
2024-11-19 17:43:24 -0500

    Footer

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer class="footer">
-      <p class="footer__author">Joanna Hazel &#169; 2024</p>
+      <p class="footer__author">Team Name &#169; 2024</p>
     </footer>
   );
 };
