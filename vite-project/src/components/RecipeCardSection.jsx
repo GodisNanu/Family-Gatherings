81cfa1e920cee1c0e5dd423d22e781baaac998fd
Godis Nanu <godisnanu@gmail.com>
2024-11-19 17:39:21 -0500

    starting markup for carousel

---
+++ vite-project/src/components/RecipeCardSection.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export const RecipeCardSection = () => {
+  return (
+    <>
+      <div className="recipe__section">
+        <div className="recipe__carousel">
+          <h1 className="recipe__carousel-header"> Choose A Dish to Bring </h1>
+        </div>
+      </div>
+    </>
+  );
+};
