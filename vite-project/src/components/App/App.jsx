0385fe4f1efb81f194c39b57b3fb22a0ed1b91fd
Imani Walker <imaniwalker0305@gmail.com>
2024-11-20 02:52:07 -0500

    style update

--- vite-project/src/components/App/App.jsx
+++ vite-project/src/components/App/App.jsx
@@ -54,7 +54,7 @@ function App() {
         isOpen={activeModal === "modal-form"}
         handleCreate={handleCreate}
       />
-      <RecipeCardSection />
+      {/* <RecipeCardSection /> */}
     </div>
   );
 }
